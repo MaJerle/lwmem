cb0ef2a035b8c3363130f200c92b4e296acdb90e
Tilen Majerle <tilen@majerle.eu>
2024-03-19 21:34:24 +0100

    Update library CMake system, to support automatic creation of options file, if user doesn't provide one

--- lwmem/library.cmake
+++ lwmem/library.cmake
@@ -1,14 +1,19 @@
 # 
+# LIB_PREFIX: LWMEM
+#
 # This file provides set of variables for end user
 # and also generates one (or more) libraries, that can be added to the project using target_link_libraries(...)
 #
 # Before this file is included to the root CMakeLists file (using include() function), user can set some variables:
 #
 # LWMEM_SYS_PORT: If defined, it will include port source file from the library.
-# LWMEM_OPTS_DIR: If defined, it should set the folder path where options file shall be generated.
+# LWMEM_OPTS_FILE: If defined, it is the path to the user options file. If not defined, one will be generated for you automatically
 # LWMEM_COMPILE_OPTIONS: If defined, it provide compiler options for generated library.
 # LWMEM_COMPILE_DEFINITIONS: If defined, it provides "-D" definitions to the library build
 #
+
+# Custom include directory
+set(LWMEM_CUSTOM_INC_DIR ${CMAKE_CURRENT_BINARY_DIR}/lib_inc)
 
 # Library core sources
 set(lwmem_core_SRCS 
@@ -23,6 +28,7 @@ set(lwmem_core_cpp_SRCS
 # Setup include directories
 set(lwmem_include_DIRS
     ${CMAKE_CURRENT_LIST_DIR}/src/include
+    ${LWMEM_CUSTOM_INC_DIR}
 )
 
 # Add system port
@@ -46,7 +52,12 @@ target_include_directories(lwmem_cpp INTERFACE ${lwmem_include_DIRS})
 target_compile_options(lwmem_cpp PRIVATE ${LWMEM_COMPILE_OPTIONS})
 target_compile_definitions(lwmem_cpp PRIVATE ${LWMEM_COMPILE_DEFINITIONS})
 
-# Create config file
-if(DEFINED LWMEM_OPTS_DIR AND NOT EXISTS ${LWMEM_OPTS_DIR}/lwmem_opts.h)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/src/include/lwmem/lwmem_opts_template.h ${LWMEM_OPTS_DIR}/lwmem_opts.h COPYONLY)
+# Create config file if user didn't provide one info himself
+if(NOT LWMEM_OPTS_FILE)
+    message(STATUS "Using default lwmem_opts.h file")
+    set(LWMEM_OPTS_FILE ${CMAKE_CURRENT_LIST_DIR}/src/include/lwmem/lwmem_opts_template.h)
+else()
+    message(STATUS "Using custom lwmem_opts.h file from ${LWMEM_OPTS_FILE}")
 endif()
+configure_file(${LWMEM_OPTS_FILE} ${LWMEM_CUSTOM_INC_DIR}/lwmem_opts.h COPYONLY)
+
